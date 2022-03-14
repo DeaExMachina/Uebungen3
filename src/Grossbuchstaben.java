bff702ad37dec98c848408f25b5edaa0679576b1
DeaExMachina <sister_of_night@gmx.at>
2022-03-14 14:07:55 +0100

    -

--- src/Grossbuchstaben.java
+++ src/Grossbuchstaben.java
@@ -1,28 +1,28 @@
-public class Grossbuchstaben {
-    public static void main(String[] args) {
-
-
-    }
-
-    public static String toUpperCase (String sentence) {
-        sentence = "Wörter starten mit großbuchstaben!";
-        char upper = sentence.charAt(8);
-        String newSentence;
-
-        while (upper < sentence.length()) {
-            upper = sentence.charAt(8);
-            newSentence = sentence + Character.toUpperCase(upper);
-        }
-            return newSentence;
-
-//        while (sentence = sentence.length()) {
-//            upper = sentence.charAt(8);
-//            sentence.charAt(16);
-//            sentence.charAt(20);
+//public class Grossbuchstaben {
+//    public static void main(String[] args) {
 //
-//            if ((sentence.charAt(8)) || (sentence.charAt(16)) || ((sentence.charAt(20)){
-//               newSentence = sentence + Character.toUpperCase(upper);
-
-    }
-
-}
+//
+//    }
+//
+//    public static String toUpperCase (String sentence) {
+//        sentence = "Wörter starten mit großbuchstaben!";
+//        char upper = sentence.charAt(8);
+//        String newSentence;
+//
+//        while (upper < sentence.length()) {
+//            upper = sentence.charAt(8);
+//            newSentence = sentence + Character.toUpperCase(upper);
+//        }
+//            return newSentence;
+//
+////        while (sentence = sentence.length()) {
+////            upper = sentence.charAt(8);
+////            sentence.charAt(16);
+////            sentence.charAt(20);
+////
+////            if ((sentence.charAt(8)) || (sentence.charAt(16)) || ((sentence.charAt(20)){
+////               newSentence = sentence + Character.toUpperCase(upper);
+//
+//    }
+//
+//}
