2519f22db813b05f5e1fd78890f8c30b8681e35e
DeaExMachina <sister_of_night@gmx.at>
2022-03-14 14:38:47 +0100

    -

--- src/Bahnapp.java
+++ src/Bahnapp.java
@@ -1,25 +1,25 @@
-public class Bahnapp {
-    public static void main(String[] args) {
-        bisZiel(1);
-
-
-    }
-
-    public static int bisZiel (int station) {
-        int minuten = 20;
-        int zeit;
-
-        while (station <= 5) {
-            System.out.println("Bei Station " + station + " sind es noch " + minuten + " Minuten.");
-            zeit = minuten - 4;
-            System.out.println("Bei Station " + station + " sind es noch " + minuten + " Minuten.");
-            zeit = minuten - 1;
-            System.out.println("Bei Station " + station + " sind es noch " + minuten + " Minuten.");
-            zeit = minuten - 5;
-            System.out.println("Bei Station " + station + " sind es noch " + minuten + " Minuten.");
-            zeit = minuten -7;
-        }
-        return zeit;
-
-    }
-}
+//public class Bahnapp {
+//    public static void main(String[] args) {
+//        bisZiel(1);
+//
+//
+//    }
+//
+//    public static int bisZiel (int station) {
+//        int minuten = 20;
+//        int zeit;
+//
+//        while (station <= 5) {
+//            System.out.println("Bei Station " + station + " sind es noch " + minuten + " Minuten.");
+//            zeit = minuten - 4;
+//            System.out.println("Bei Station " + station + " sind es noch " + minuten + " Minuten.");
+//            zeit = minuten - 1;
+//            System.out.println("Bei Station " + station + " sind es noch " + minuten + " Minuten.");
+//            zeit = minuten - 5;
+//            System.out.println("Bei Station " + station + " sind es noch " + minuten + " Minuten.");
+//            zeit = minuten -7;
+//        }
+//        return zeit;
+//
+//    }
+//}
---
+++ src/Loops.java
@@ -0,0 +1,16 @@
+public class Loops {
+    public static void main(String[] args) {
+        int zahl = 1;
+//        for (zahl = 1; zahl <= 100; zahl++) {
+//            System.out.println(zahl);
+//        }
+
+        while (zahl <=100) {
+            System.out.println(zahl);
+            zahl++;
+        }
+
+
+    }
+}
+
