eef1c10193797c3b233fe31e6caa5ac73355af8f
DeaExMachina <sister_of_night@gmx.at>
2022-03-16 13:07:05 +0100

    -

--- src/NarcissticNumbers.java
+++ src/NarcissticNumbers.java
@@ -1,5 +1,21 @@
 public class NarcissticNumbers {
     public static void main(String[] args) {
+        for (int num = 1; num <= 1000; num++) {
+            int number, digit, sum = 0;
+            number = num;
 
+            while (number != 0) {
+                digit = number % 10;
+                sum = sum + digit * digit * digit;
+                number /= 10;
+            }
+
+            if (sum == num) {
+                System.out.println(num + " is an Armstrong number.");
+            }
+//            else {
+//                System.out.println(num + " is not an Armstrong number.");
+//            }
+        }
     }
 }
