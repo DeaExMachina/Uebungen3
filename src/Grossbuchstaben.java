d317e0549da0ab1b199380c91490ef158fea681b
DeaExMachina <sister_of_night@gmx.at>
2022-03-17 10:11:23 +0100

    -

--- src/Grossbuchstaben.java
+++ src/Grossbuchstaben.java
@@ -1,28 +1,36 @@
-//public class Grossbuchstaben {
-//    public static void main(String[] args) {
-//
-//
-//    }
-//
-//    public static String toUpperCase (String sentence) {
-//        sentence = "Wörter starten mit großbuchstaben!";
-//        char upper = sentence.charAt(8);
-//        String newSentence;
-//
-//        while (upper < sentence.length()) {
+public class Grossbuchstaben {
+    public static void main(String[] args) {
+
+        System.out.println(toUpperCase("I want some food and i want it now!"));
+        System.out.println(toUpperCase("If you can read this, bring me coffee."));
+
+
+    }
+
+    public static String toUpperCase (String sentence) {
+        String newSentence = new String();
+        char c = ' ';
+        newSentence = newSentence + sentence.charAt(0);
+
+        for (int i = 1; i < sentence.length(); i++) {
+            c = sentence.charAt(i);
+
+            if (sentence.charAt(i - 1 ) == ' ') {
+                c = Character.toUpperCase(sentence.charAt(i));
+            }
+
+            newSentence = newSentence + c;
+        }
+            return newSentence;
+
+//        while (sentence = sentence.length()) {
 //            upper = sentence.charAt(8);
-//            newSentence = sentence + Character.toUpperCase(upper);
-//        }
-//            return newSentence;
+//            sentence.charAt(16);
+//            sentence.charAt(20);
 //
-////        while (sentence = sentence.length()) {
-////            upper = sentence.charAt(8);
-////            sentence.charAt(16);
-////            sentence.charAt(20);
-////
-////            if ((sentence.charAt(8)) || (sentence.charAt(16)) || ((sentence.charAt(20)){
-////               newSentence = sentence + Character.toUpperCase(upper);
-//
-//    }
-//
-//}
+//            if ((sentence.charAt(8)) || (sentence.charAt(16)) || ((sentence.charAt(20)){
+//               newSentence = sentence + Character.toUpperCase(upper);
+
+    }
+
+}
---
+++ src/SortingArrays.java
@@ -0,0 +1,13 @@
+//public class SortingArrays {
+//    public static void main(String[] args) {
+//
+//    }
+//    public static void arrangeOrder (int[] numbers) {
+//        numbers = new int[] {8, 5, 1, 9, 0, 4};
+//
+//        while () {
+//
+//        }
+//
+//    }
+//}
