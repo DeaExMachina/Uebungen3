13baecd92da30ff07d7c1e344185764a754e34e6
DeaExMachina <sister_of_night@gmx.at>
2022-03-22 12:47:07 +0100

    -

--- src/NumberHelper.java
+++ src/NumberHelper.java
@@ -1,11 +1,19 @@
+
+
 public class NumberHelper {
+    public static void main(String[] args) {
+
+        int [][] numbers = new int[][] {{0, 1, 9},{6, 3, 2}};
+        getFlatArray(numbers);
+    }
     public static int[] getFlatArray(int[][] numbers) {
-        numbers = new int[][] {{0, 1, 9},{6, 3, 2}};
+
 
         //soll in ein eindimensionales Array umgewandelt werden
 
         int counter = 0;
-        int [] numberArray = new int[6];
+        int [] numberArray = new int[numbers[0].length * numbers.length];
+                            // Anzahl der Spalten     mal    Anzahl der Zeilen, um alle Zahlen ins flat array zu bekommen
 
         for (int i = 0; i < numbers.length; i++) {
             for (int j = 0; j < numbers[i].length; j++){
@@ -14,5 +22,8 @@ public class NumberHelper {
                 counter++;
             }
         }
+        return numberArray;
     }
 }
+
+
